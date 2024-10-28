ca0e643e5a2d7a0a1308a48029595886e2145b26
Mark kimaaathi <markangelkimathi@gmail.com>
2024-10-28 22:01:26 +0300

    Update About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -22,8 +22,9 @@ const About = () => {
 
       <div className='mt-5 flex flex-col gap-3 text-slate-500'>
         <p>
-          Software Developer based in kenya, specializing in technical
-          education through hands-on learning and building applications.
+        Software Developer based in Kenya, specializing in hands-on learning,
+        building applications, open-source configuration, 
+        Artificial Intelligence and Machine learning.
         </p>
       </div>
 
